66fe9c9dcf86fbea2b0b974267c7e8c1085b077e
Alex Abdugafarov <alex@yugabyte.com>
2019-05-20 16:56:26 -0700

    Updated Jepsen dependency, added NOOP workload

--- yugabyte/src/yugabyte/core.clj
+++ yugabyte/src/yugabyte/core.clj
@@ -3,6 +3,7 @@
   (:require [clojure.tools.logging :refer :all]
             [clojure.string :as str]
             [jepsen [checker :as checker]
+                    [client :as client]
                     [generator :as gen]
                     [tests :as tests]]
             [jepsen.control.util :as cu]
@@ -15,15 +16,17 @@
                       [multi-key-acid :as multi-key-acid]
                       [nemesis :as nemesis]
                       [single-key-acid :as single-key-acid]
-                      [set :as set]]))
+                      [set :as set]]) (:use [clojure.pprint]))
 
 (def workloads
   "A map of workload names to functions that can take option maps and construct
   workloads."
-  {:bank              bank/workload
+  {:none              (fn [opts] (merge tests/noop-test opts)) ; Just setup and teardown the DB
+   :bank              bank/workload
    :bank-multitable   bank/multitable-workload
    :counter           counter/workload
    :long-fork         long-fork/workload
+
    :multi-key-acid    multi-key-acid/workload
    :set               set/workload
    :set-index         set/index-workload
@@ -150,44 +153,49 @@
   "Second phase of test construction. Builds the workload and nemesis, and
   finalizes the test."
   [opts]
-  (let [workload  ((get workloads (:workload opts)) opts)
-        nemesis   (nemesis/nemesis opts)
-        gen       (->> (:generator workload)
-                       (gen/nemesis (:generator nemesis))
-                       (gen/time-limit (:time-limit opts)))
-        gen       (if (:final-generator workload)
-                    (gen/phases gen
-                                (gen/log "Healing cluster")
-                                (gen/nemesis (:final-generator nemesis))
-                                (gen/log "Waiting for recovery...")
-                                (gen/sleep (:final-recovery-time opts))
-                                (gen/clients (:final-generator workload)))
-                    gen)
-        perf (checker/perf
-               {:nemeses #{{:name  "kill master"
-                            :start #{:kill-master :stop-master}
-                            :stop  #{:start-master}
-                            :fill-color "#E9A4A0"}
-                           {:name  "kill tserver"
-                            :start #{:kill-tserver :stop-tserver}
-                            :stop  #{:start-tserver}
-                            :fill-color "#E9C3A0"}
-                           {:name  "pause master"
-                            :start #{:pause-master}
-                            :stop  #{:resume-master}
-                            :fill-color "#A0B1E9"}
-                           {:name  "pause tserver"
-                            :start #{:pause-tserver}
-                            :stop  #{:resume-tserver}
-                            :fill-color "#B8A0E9"}
-                           {:name "clock skew"
-                            :start #{:bump-clock :strobe-clock}
-                            :stop  #{:reset-clock}
-                            :fill-color "#D2E9A0"}
-                           {:name "partition"
-                            :start #{:start-partition}
-                            :stop  #{:stop-partition}
-                            :fill-color "#888888"}}})]
+  (let [workload ((get workloads (:workload opts)) opts)
+        nemesis  (nemesis/nemesis opts)
+        gen      (->> (:generator workload)
+                      (gen/nemesis (:generator nemesis))
+                      (gen/time-limit (:time-limit opts)))
+        gen      (if (:final-generator workload)
+                   (gen/phases gen
+                               (gen/log "Healing cluster")
+                               (gen/nemesis (:final-generator nemesis))
+                               (gen/log "Waiting for recovery...")
+                               (gen/sleep (:final-recovery-time opts))
+                               (gen/clients (:final-generator workload)))
+                   gen)
+        perf     (checker/perf
+                   {:nemeses #{{:name       "kill master"
+                                :start      #{:kill-master :stop-master}
+                                :stop       #{:start-master}
+                                :fill-color "#E9A4A0"}
+                               {:name       "kill tserver"
+                                :start      #{:kill-tserver :stop-tserver}
+                                :stop       #{:start-tserver}
+                                :fill-color "#E9C3A0"}
+                               {:name       "pause master"
+                                :start      #{:pause-master}
+                                :stop       #{:resume-master}
+                                :fill-color "#A0B1E9"}
+                               {:name       "pause tserver"
+                                :start      #{:pause-tserver}
+                                :stop       #{:resume-tserver}
+                                :fill-color "#B8A0E9"}
+                               {:name       "clock skew"
+                                :start      #{:bump-clock :strobe-clock}
+                                :stop       #{:reset-clock}
+                                :fill-color "#D2E9A0"}
+                               {:name       "partition"
+                                :start      #{:start-partition}
+                                :stop       #{:stop-partition}
+                                :fill-color "#888888"}}})
+        checker  (if (= (:workload opts) :none)
+                   (:checker workload)
+                   (checker/compose {:perf     perf
+                                     :clock    (checker/clock-plot)
+                                     :workload (:checker workload)}))]
     (merge tests/noop-test
            opts
            (dissoc workload
@@ -196,12 +204,10 @@
                    :checker)
            (when (:yugabyte-ssh opts) (yugabyte-ssh-defaults))
            (when (:trace-cql opts)    (trace-logging))
-           {:client     (:client workload)
-            :nemesis    (:nemesis nemesis)
-            :generator  gen
-            :checker    (checker/compose {:perf     perf
-                                          :clock    (checker/clock-plot)
-                                          :workload (:checker workload)})})))
+           {:client    (:client workload)
+            :nemesis   (:nemesis nemesis)
+            :generator gen
+            :checker   checker})))
 
 (defn yb-test
   "Constructs a yugabyte test from CLI options."
