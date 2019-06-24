cf3a0ef28ace718753840cfd777e7e7e3193a788
Alex Abdugafarov <alex@yugabyte.com>
2019-06-24 21:55:19 +0500

    More comments on single-key-acid

--- yugabyte/src/yugabyte/single_key_acid.clj
+++ yugabyte/src/yugabyte/single_key_acid.clj
@@ -1,8 +1,17 @@
 (ns yugabyte.single-key-acid
   "Given a single table of hash column primary key and one value column with
-  (value of --concurrency divided by 2 and by # of nodes) rows,
+  (value of --concurrency divided by 2 and by # of nodes) independent rows,
   verify that concurrent reads, writes and read-modify-write (UPDATE IF) operations
-  results in linearizable history"
+  results in linearizable history.
+
+  Here's the deal. Each group of 2N consequent threads allocated by --concurrency are assigned
+  to a separate row key. Of these 2N threads, first N are performing writes/updates and
+  the last N are reading current state. Thread groups (i.e. table rows) are completely independent.
+  To illustrate this further, given --concurrency 20 and N = 5:
+  - Threads  0 to  9 will be working with row #0
+  - Threads 10 to 19 will be working with row #1
+  - Threads 0 to 4 and 10 to 14 will be updating their respective rows
+  - Threads 5 to 9 and 15 to 19 will be reading their respective rows"
   (:require [clojure [pprint :refer :all]]
             [clojure.tools.logging :refer [debug info warn]]
             [jepsen.client :as client]
