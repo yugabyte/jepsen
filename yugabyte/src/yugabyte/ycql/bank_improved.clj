6068c4fa48866581b4e864de6c73a89893ca1a2d
Dmitry Sherstobitov <dsherstobitov@yugabyte.com>
2021-10-28 15:57:05 +0300

    Fixed YCQL

--- yugabyte/src/yugabyte/ycql/bank_improved.clj
+++ yugabyte/src/yugabyte/ycql/bank_improved.clj
@@ -4,13 +4,9 @@
   (:require [clojure.tools.logging :refer [debug info warn]]
             [clojurewerkz.cassaforte.client :as cassandra]
             [clojurewerkz.cassaforte.cql :as cql]
-            [yugabyte.bank-improved :as bank-improved]
-            [clojurewerkz.cassaforte.query
-             :as q
-             :refer :all]
+            [clojurewerkz.cassaforte.query :as q :refer :all]
             [yugabyte.ycql.client :as c]))
 
-(def setup-lock (Object.))
 (def keyspace "jepsen")
 (def table-name "accounts")
 
@@ -33,47 +29,40 @@
                     {:id a, :balance 0}))))
 
   (invoke! [this test op]
-    (case (:f op)
-      :read
-      (c/with-errors
-        op #{:read}
+    (c/with-errors
+      op #{:read}
+      (case (:f op)
+        :read
         (->> (cql/select-with-ks conn keyspace table-name)
              (map (juxt :id :balance))
              (into (sorted-map))
-             (assoc op :type :ok, :value)))
+             (assoc op :type :ok, :value))
 
-      :update
-      (c/with-errors
-        op #{:read}
+        :update
         (let [{:keys [from to amount]} (:value op)]
-          (do
-            (cassandra/execute
-              conn
-              ; TODO: separate reads from updates?
-              (str "BEGIN TRANSACTION "
-                   "UPDATE " keyspace "." table-name
-                   " SET balance = balance - " amount " WHERE id = " from ";"
+          (cassandra/execute
+            conn
+            ; TODO: separate reads from updates?
+            (str "BEGIN TRANSACTION "
+                 "UPDATE " keyspace "." table-name
+                 " SET balance = balance - " amount " WHERE id = " from ";"
 
-                   "UPDATE " keyspace "." table-name
-                   " SET balance = balance + " amount " WHERE id = " to ";"
-                   "END TRANSACTION;"))
-            (assoc op :type :ok :value {:from from, :to to, :amount amount}))))
+                 "UPDATE " keyspace "." table-name
+                 " SET balance = balance + " amount " WHERE id = " to ";"
+                 "END TRANSACTION;"))
+          (assoc op :type :ok))
 
-
-      :insert
-      (c/with-errors
-        op #{:read}
+        :insert
         (let [{:keys [from to amount]} (:value op)]
-          (do
-            (cassandra/execute
-              conn
-              (str "BEGIN TRANSACTION "
-                   "INSERT INTO " keyspace "." table-name
-                   " (id, balance) values (" to "," amount ");"
+          (cassandra/execute
+            conn
+            (str "BEGIN TRANSACTION "
+                 "INSERT INTO " keyspace "." table-name
+                 " (id, balance) values (" to "," amount ");"
 
-                   "UPDATE " keyspace "." table-name
-                   " SET balance = balance + " amount " WHERE id = " from ";"
-                   "END TRANSACTION;"))
-            (assoc op :type :ok :value {:from from, :to to, :amount amount}))))))
+                 "UPDATE " keyspace "." table-name
+                 " SET balance = balance - " amount " WHERE id = " from ";"
+                 "END TRANSACTION;"))
+          (assoc op :type :ok)))))
 
   (teardown! [this test]))
