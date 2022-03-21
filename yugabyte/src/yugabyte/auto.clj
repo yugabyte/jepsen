5211e24dc41e777f9444a35dd69dcebd76bd39f3
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2022-03-21 16:53:22 +0300

    Disabled READ COMMITED tests till issue will be released

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -12,8 +12,7 @@
             [jepsen.control.net :as cn]
             [jepsen.control.util :as cu]
             [jepsen.os.debian :as debian]
-            [jepsen.os.centos :as centos]
-            [jepsen.reconnect :as rc]
+            [version-clj.core :as v]
             [yugabyte.ycql.client :as ycql.client]
             [yugabyte.ysql.client :as ysql.client]
             [slingshot.slingshot :refer [try+ throw+]])
@@ -27,6 +26,8 @@
 (def master-log-dir  (str dir "/master/logs"))
 (def tserver-log-dir (str dir "/tserver/logs"))
 (def installed-url-file (str dir "/installed-url"))
+
+(def minimal-read-committed-version "2.13.1.0-b1")
 
 (def max-bump-time-ops-per-test
   "Upper bound on number of bump time ops per test, needed to estimate max
@@ -409,6 +410,7 @@
             :--rpc_slow_query_threshold_ms 1000
             :--load_balancer_max_concurrent_adds 10
             (tserver-api-opts (:api test) node)
+            (tserver-version-specific test)
 
             ; Heartbeats
             ;:--heartbeat_interval_ms 100
@@ -419,7 +421,7 @@
             ;:--leader_failure_max_missed_heartbeat_period 3
             ;:--consensus_rpc_timeout_ms 300
             ;:--client_read_write_timeout_ms 6000
-            )))
+                                      )))
 
   (stop-master! [db]
     (c/su (cu/stop-daemon! ce-master-bin ce-master-pidfile)))
