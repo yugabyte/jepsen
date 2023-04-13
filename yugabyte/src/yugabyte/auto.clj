0d7c2304893ed17a9f3731f39788a1f92046963e
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-04-14 00:20:26 +0400

    WIP

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -201,10 +201,10 @@
 
   (if (clojure.string/includes? (name (:workload test)) "geo.")
     (do
-      (yugabyte.auto/yb-admin test :create_transaction_table :jepsen_1_transactions)
-      (yugabyte.auto/yb-admin test :modify_table_placement_info :system :jepsen_1_transactions "gcp.jepsen-1.jepsen-1a" 3)
-      (yugabyte.auto/yb-admin test :create_transaction_table :jepsen_2_transactions)
-      (yugabyte.auto/yb-admin test :modify_table_placement_info :system :jepsen_2_transactions "gcp.jepsen-2.jepsen-2a" 3)))
+      (yugabyte.auto/yb-admin test :create_transaction_table :transactions_jepsen_1)
+      (yugabyte.auto/yb-admin test :modify_table_placement_info :system :transactions_jepsen_1 "gcp.jepsen-1.jepsen-1a" 3)
+      (yugabyte.auto/yb-admin test :create_transaction_table :transactions_jepsen_2)
+      (yugabyte.auto/yb-admin test :modify_table_placement_info :system :transactions_jepsen_2 "gcp.jepsen-2.jepsen-2a" 3)))
 
   :started)
 
