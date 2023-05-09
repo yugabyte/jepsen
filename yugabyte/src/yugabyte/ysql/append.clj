0b56f11fc1212b838e454a165dcba6e04d034c33
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-05-09 11:59:46 +0400

    Debug enabled flags

--- yugabyte/src/yugabyte/ysql/append.clj
+++ yugabyte/src/yugabyte/ysql/append.clj
@@ -193,8 +193,9 @@
   [c table tablespace-name postfix]
   (info (str "Create table partitions for " table "_" postfix " for '" postfix "'"))
   (c/execute! c (str "CREATE TABLE " table "_" postfix " "
-                     "PARTITION OF " table " (k, k2, geo_partition, "
-                     "PRIMARY KEY (k, geo_partition)) FOR VALUES IN ('" postfix "') "
+                     "PARTITION OF " table " (k, k2, geo_partition"
+                     ;", PRIMARY KEY (k, geo_partition)) FOR VALUES IN ('" postfix "') "
+                     ") FOR VALUES IN ('" postfix "') "
                      "TABLESPACE " tablespace-name "_" postfix
                      )))
 
