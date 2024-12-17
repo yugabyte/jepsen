cbc24a11109e1f04c721ba3f0214f922dc6271bd
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2024-12-17 11:59:40 -0800

    Random connection manager usage

--- yugabyte/src/yugabyte/runner.clj
+++ yugabyte/src/yugabyte/runner.clj
@@ -57,7 +57,7 @@
     :default false]
 
    [nil "--connection-manager" "Enable connection manager flags on YB since 2024.2 version"
-    :default true]
+    :default (rand [true false])]
 
    [nil "--clock-skew-flags" "Enable soft clock skew flags on YB"
     :default true]
