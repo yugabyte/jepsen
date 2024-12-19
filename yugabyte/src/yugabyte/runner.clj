76ccea44571b0153fe9ee81558350960800d86bb
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2024-12-18 19:00:33 -0800

    GPT knows Clojure better

--- yugabyte/src/yugabyte/runner.clj
+++ yugabyte/src/yugabyte/runner.clj
@@ -57,7 +57,7 @@
     :default false]
 
    [nil "--connection-manager" "Enable connection manager flags on YB since 2024.2 version"
-    :default (rand [true false])]
+    :default (rand-nth [true false])]
 
    [nil "--clock-skew-flags" "Enable soft clock skew flags on YB"
     :default true]
