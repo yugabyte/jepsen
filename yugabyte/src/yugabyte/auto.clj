d772e5d6f74de477d772e90e0a662334b76aceb1
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2025-01-07 17:18:47 -0800

    Create permission WA

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -608,11 +608,12 @@
         (ysqlsh test :-p port :-h (cn/ip node) :-c (str "DROP DATABASE IF EXISTS jepsen;"))
         (ysqlsh test :-p port :-h (cn/ip node) :-c (str "CREATE DATABASE jepsen" colocated-clause ";"))
         (ysqlsh test :-p port :-h (cn/ip node) :-c (str "DROP USER IF EXISTS jepsen;
-                                                CREATE USER jepsen;
+                                                CREATE USER jepsen createdb;
                                                 ALTER USER jepsen WITH PASSWORD 'jepsen';
                                                 GRANT ALL ON DATABASE jepsen TO jepsen;
                                                 GRANT ALL ON ALL TABLES IN SCHEMA public TO jepsen;
                                                 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO jepsen;
+                                                GRANT CREATE ON SCHEMA public TO public;
                                                 GRANT ALL ON SCHEMA public TO jepsen;"))
         (if (str/includes? (:name test) ".geo.")
           (do
