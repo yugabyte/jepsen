ab8b9ac93af056fe702abc5ab54fde0883b5bcf9
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2025-06-18 14:02:22 -0700

    Added append table into allowed workloads

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -535,8 +535,6 @@
             (ce-shared-opts node)
             :--master_addresses (master-addresses test)
             :--replication_factor (:replication-factor test)
-            :--allowed_preview_flags_csv "enable_ysql_conn_mgr"
-            :--enable_ysql_conn_mgr
             ;:--auto_create_local_transaction_tables=false
             (master-tserver-experimental-tuning-flags test)
             (master-tserver-random-clock-skew test node)
