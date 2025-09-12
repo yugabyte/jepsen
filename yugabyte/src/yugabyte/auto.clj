631e7915842077a24529515356c1c20544cfe35f
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2025-09-12 10:03:44 -0700

    Skiplock

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -558,6 +558,8 @@
             :--tserver_master_addrs (master-addresses test)
             ; Tracing
             :--enable_tracing
+            :--allowed_preview_flags_csv "skip_prefix_locks"
+            :--skip_prefix_locks "true"
             :--rpc_slow_query_threshold_ms 1000
             (master-tserver-experimental-tuning-flags test)
             (master-tserver-random-clock-skew test node)
