2034af05920bf802601ee7909d24dc6033e6c91c
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2026-01-26 04:19:42 -0800

    WIP

--- yugabyte/project.clj
+++ yugabyte/project.clj
@@ -13,6 +13,6 @@
                  [version-clj "2.0.2"]
                  [clj-wallhack "1.0.1"]]
   :main yugabyte.runner
-  :jvm-opts ["-Djava.awt.headless=true"])
+  :jvm-opts ["-Djava.awt.headless=true" "-Xms2g" "-Xmx4g"])
 ;  :aot [yugabyte.runner
 ;        clojure.tools.logging.impl])
