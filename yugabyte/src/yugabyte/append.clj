9560fae4aebee5905e1282b9a51ed2f5f52f6ed6
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2026-02-04 11:55:31 -0800

    Updated `workload-

--- yugabyte/src/yugabyte/append.clj
+++ yugabyte/src/yugabyte/append.clj
@@ -21,7 +21,8 @@
   (-> (append/test {:key-count          32
                     :max-txn-length     4
                     :max-writes-per-key 512
-                    :anomalies          [:G1a :G1b]
+                    :anomalies          [:G0 :G1a :G1b]  ; only dirty writes and dirty reads
+                    :consistency-models nil
                     :additional-graphs  []})))
 
 (defn workload-serializable
