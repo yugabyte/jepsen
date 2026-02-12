8ebc59828a4819a004dc0273f1bec81a31fc6b31
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2026-02-11 17:34:13 -0800

    Set `JAVA_HOME` in Jepsen test runner to `/usr/lib/jvm/zulu-25.jdk`.

--- yugabyte/run-jepsen.py
+++ yugabyte/run-jepsen.py
@@ -443,6 +443,7 @@ def main():
     connection_manager_flag = "--connection-manager false" \
         if not (is_version_at_least("2024.1.0.0-b1", version) or
                 is_version_at_least("2.25.1.0-b1", version)) else ""
+    os.environ["JAVA_HOME"] = "/usr/lib/jvm/zulu-25.jdk"
     lein_cmd = " ".join(["lein run test",
                          "--os debian",
                          f"--url {url}",
