55ed8c45a584cb85daf49c2617473b5a057e3090
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2026-03-25 10:06:00 -0700

    WIP

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -511,13 +511,13 @@
   "Stress-test flags for tserver — DocDB, RocksDB, MVCC, intent cleanup."
   [test]
   (if (:stress-tuning test)
-    [:--txn_max_apply_batch_records 2
+    [:--txn_max_apply_batch_records 5
      ; :--db_write_buffer_size 524288
      ; :--db_block_cache_size_bytes 8388608
-     :--aborted_intent_cleanup_ms 1000
-     :--timestamp_history_retention_interval_sec 5
-     :--transaction_deadlock_detection_interval_usec 1000000
-     :--backfill_index_write_batch_size 2
+;     :--aborted_intent_cleanup_ms 1000
+;     :--timestamp_history_retention_interval_sec 5
+;     :--transaction_deadlock_detection_interval_usec 1000000
+     :--backfill_index_write_batch_size 10
      ; :--cdc_stream_records_threshold_size_bytes 1024
      ]
     []))
