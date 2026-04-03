b1558d0a206b6e47f51da43336016db6087610dc
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2026-04-03 16:17:27 -0700

    Remove redundant concurrency adjustment logic for serializable workloads in `assoc-workload-opts`.

--- yugabyte/run-jepsen.py
+++ yugabyte/run-jepsen.py
@@ -568,7 +568,7 @@ def main():
             if 'append-table' in test:
                 concurrency = '3'
             elif '/sz.' in test:
-                concurrency = '3n'
+                concurrency = '1n'
             else:
                 concurrency = args.concurrency
             full_cmd = lein_cmd + \
