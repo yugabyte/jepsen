73cba85fc5332690eaeec6f1e01eac00c7aa32c6
Shubham Dabriwala <shubhamdabriwala01@gmail.com>
2026-09-01 10:31:48 +0530

    Added vmodule read_query=2 for better debugging (#137)

--- yugabyte/src/yugabyte/auto.clj
+++ yugabyte/src/yugabyte/auto.clj
@@ -759,7 +759,7 @@
                (master-stress-flags test)
                (master-append-table-flags test)
                ; Debug logging for DDL correctness issues
-               :--vmodule "pg_client_session=4,async_rpc=2"
+               :--vmodule "pg_client_session=4,async_rpc=2,read_query=2"
                (master-api-opts (:api test) node)]
               (:master-flags test)))))
 
@@ -792,7 +792,7 @@
                (tserver-heartbeat-flags test)
                ; Debug logging for DDL correctness issues. ysql_pg_conf_csv is
                ; tserver-only.
-               :--vmodule "pg_client_session=4,async_rpc=2"
+               :--vmodule "pg_client_session=4,async_rpc=2,read_query=2"
                :--ysql_pg_conf_csv "log_statement=all,yb_debug_log_internal_restarts=true"]
               (:tserver-flags test)))))
 
